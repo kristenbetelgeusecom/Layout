b4a57be325f92b625be0d7c0007e669d887e7337
Кристина Бабич <betelgeuse@MacBook-Pro-Kristina.local>
2021-12-04 21:42:01 +0300

    CT-6: finish 1

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let body = document.querySelector('body')
 
 let openreview1 = document.querySelector('.openReview1')
 let review1 = document.querySelector('.review1')
+let isOpenReview1 = false
+let isOpenReview2 = false
+let isOpenReview3 = false
 
 let openreview2 = document.querySelector('.openReview2')
 let review2 = document.querySelector('.review2')
@@ -28,18 +31,37 @@ clickServices.addEventListener('click', function () {
 })
 
 openreview1.addEventListener('click', function () {
-  review1.style.display = 'block'
-  openreview1.innerHTML = 'Свернуть отзыв'
+  if (isOpenReview1 == false) {
+    review1.style.display = 'block'
+    openreview1.innerHTML = 'Свернуть отзыв'
+    isOpenReview1 = true
+  } else {
+    review1.style.display = 'none'
+    openreview1.innerHTML = 'Читать полностью'
+    isOpenReview1 = false
+  }
 })
+
 openreview2.addEventListener('click', function () {
-  review2.style.display = 'block'
-  openreview2.innerHTML = 'Свернуть отзыв'
+  if (isOpenReview2 == false) {
+    review2.style.display = 'block'
+    openreview2.innerHTML = 'Свернуть отзыв'
+    isOpenReview2 = true
+  } else {
+    review2.style.display = 'none'
+    openreview2.innerHTML = 'Читать полностью'
+    isOpenReview2 = false
+  }
 })
+
 openreview3.addEventListener('click', function () {
-  review3.style.display = 'block'
-  openreview3.innerHTML = 'Свернуть отзыв'
+  if (isOpenReview3 == false) {
+    review3.style.display = 'block'
+    openreview3.innerHTML = 'Свернуть отзыв'
+    isOpenReview3 = true
+  } else {
+    review3.style.display = 'none'
+    openreview3.innerHTML = 'Читать полностью'
+    isOpenReview3 = false
+  }
 })
-
-
-
-
