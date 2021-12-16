2122f6aa94a56dda4705276224f16cd0078414ef
kristenbetelgeusecom <kristenbetelgereal@gmail.com>
2021-12-16 14:05:17 +0300

    CT - 7: start 2

--- script.js
+++ script.js
@@ -66,38 +66,46 @@ openreview3.addEventListener('click', function () {
   }
 })
 
-
-
-
-
-let time = 18000;
+let time = 18000 * 300;
 const countDownElement = document.querySelector('.forms-time')
+let hiddenMobile = document.querySelector('.hidden-mobile span')
+let border = document.querySelector('.forms__white-border')
 
 setInterval(updateCountdown, 1000);
 
 function updateCountdown() {
-  let hours = Math.floor(time / 3600);
-  const minutes = Math.floor(time / 300);
-  let seconds = time % 60;
-  hours = hours < 10 ? "0" + hours : hours;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-  countDownElement.innerHTML = `${hours}:${minutes}:${seconds}`;
-  time--;
+  if (time > 0) {
+    let hours = Math.floor(time / (60 * 60000));
+    let minutes = Math.floor(((time - (hours * 3600000)) / 60000) % 60);
+    let seconds = Math.floor((time - ((hours * 3600000) + (minutes * 60000))) / 1000);
+    hours = hours < 10 ? "0" + hours : hours;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    countDownElement.innerHTML = `${hours}:${minutes}:${seconds}`;
+    time = time - 1000;
+  }
+  else {
+    countDownElement.innerHTML = 'Скидка больше не действует';
+    countDownElement.style.fontSize = '2rem'
+    countDownElement.style.textAlign = 'center'
+    hiddenMobile.style.display = 'none'
+    border.style.width = '11rem'
+  }
 }
 
-const send = querySelector('.send')
-const name = querySelector('.name')
-const phone = querySelector('.phone')
+const send = document.querySelector('.send')
+const namee = document.querySelector('.name')
+const phone = document.querySelector('.phone')
 const form = document.querySelector('form');
+console.log(document)
 
 send.addEventListener('click', function (e) {
   e.preventDefault();
-  if (name.value === '' && phone.value === '') {
-    name.classList.add(error)
-    phone.classList.add(error)
+  if (namee.value === '' && phone.value === '') {
+    namee.classList.add('error')
+    phone.classList.add('error')
   }
-  if (name.value !== '' && phone.value !== '') {
-    name.classList.remove('error');
+  if (namee.value !== '' && phone.value !== '') {
+    namee.classList.remove('error');
     phone.classList.remove('error');
   }
 })
