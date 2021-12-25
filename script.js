34027f137db177bd466119cb05d62f3b2b8dfdde
kristenbetelgeusecom <kristenbetelgereal@gmail.com>
2021-12-25 22:13:56 +0300

    CT - 7: finish

--- script.js
+++ script.js
@@ -110,7 +110,6 @@ send.addEventListener('click', function (e) {
   }
 })
 
-
 const addCity = document.querySelector('.add-city')
 const cities = document.querySelector('.cities')
 
@@ -124,4 +123,48 @@ addCity.addEventListener("keypress", (event) => {
   }
 });
 addCity.value !== ""
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex)
+
+function plusSlide() {
+  slideIndex = slideIndex + 1;
+  showSlides(slideIndex);
+}
+
+function minusSlide() {
+  slideIndex = slideIndex - 1;
+  showSlides(slideIndex);
+}
+
+function curentSlide(n) {
+  slideIndex = n;
+  showSlides(slideIndex);
+}
+
+function showSlides(n) {
+  var slides = document.getElementsByClassName('item');
+  var dots = document.getElementsByClassName('slider-dots_item')
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (var i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "flex";
+  dots[slideIndex - 1].className += " active";
+}
 
